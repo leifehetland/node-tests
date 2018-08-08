dc1d80602ca10be6aaf2da0a87c9c17cdabe66b4
Leif Hetland <lehetland1@gmail.com>
2018-08-08 18:39:59 -0400

    working with expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,19 +1,47 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
 	let result = utils.add(55,10);
 
-	if (result !== 65) {
-		throw new Error(`Expected 65, but got ${result}.`);	
-	}
+	expect(result).toBe(65);
+	expect(result).toBeA('number');
+	// if (result !== 65) {
+	// 	throw new Error(`Expected 65, but got ${result}.`);	
+	// }
 	
 });
 
 it('should square a number', () => {
 	let result = utils.square(5);
 
-	if (result !== 25) {
-		throw new Error(`Expected 25, but got ${result}.`);
-	}
+	expect(result).toBe(25).toBeA('number');
+	// if (result !== 25) {
+	// 	throw new Error(`Expected 25, but got ${result}.`);
+	// }
+});
 
+it('should expect some values', () => {
+	// expect({ name: 'Leif' }).toBe({ name: 'Leif' });
+	expect({ name: 'Leif' }).toEqual({ name: 'Leif' });
+	expect({ name: 'Leif' }).toNotEqual({ name: 'leif' });
+	expect(['a','b','c']).toInclude('c');
+	expect({
+		name: 'Leif',
+		age: 28
+	}).toInclude({age: 28});
+});
+
+it('should verify first and last names are set', () => {
+
+	let user = {
+		location: 'Georgia',
+		age: 28
+	};
+	let response = utils.setName(user, 'Leif Hetland');
+
+	// expect({ name: 'Leif' }).toBe({ name: 'Leif' });
+	expect(user).toEqual(response);
+	expect(response).toInclude({ age: 28 });
+	expect(response).toInclude({ firstName: 'Leif', lastName: 'Hetland' });
 });
